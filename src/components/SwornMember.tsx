2a640b978a0199e8bef3017a3b323509a6aa03df
John Pino <iam@johnpino.me>
2024-08-27 23:02:55 -0500

    style: small styling adjustment

--- src/components/SwornMember.tsx
+++ src/components/SwornMember.tsx
@@ -10,7 +10,7 @@ const SwornMember = async (props: SwornMemberProps) => {
     return (
         <div className={`p-2 rounded-sm ${swornMember.died ? 'bg-slate-800 text-gray-400' : 'bg-slate-700'}`}>
             <h3 className="font-bold uppercase text-sm">{swornMember.name}</h3>
-            <span className="text-xs">{swornMember.died ? `Death: ${swornMember.died}` : 'Alive'}</span>
+            <span className={`text-xs ${swornMember.died ? '' : 'text-green-600'}`}>{swornMember.died ? `Death: ${swornMember.died}` : 'Alive'}</span>
         </div>
     )
 }
