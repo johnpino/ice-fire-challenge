b8fc8eb956d85fb7f5b459941c1987a63c835274
John Pino <iam@johnpino.me>
2024-08-27 22:44:13 -0500

    style: add icons

--- src/components/House.tsx
+++ src/components/House.tsx
@@ -1,4 +1,5 @@
 import SwornMember from "./SwornMember";
+import { HomeIcon } from '@heroicons/react/24/solid'
 
 type HouseProps = {
     name: string;
@@ -9,7 +10,7 @@ const House = (props: HouseProps) => {
 
     return (
         <article className="bg-slate-900 p-4">
-            <h2 className="mb-2 uppercase font-bold">{props.name}</h2>
+            <h2 className="mb-2 uppercase font-bold flex gap-2 items-center"><HomeIcon className="size-4" />{props.name}</h2>
             {props.sworMembers.length
                 ?
                 <div className="grid auto-rows-[1fr] grid-cols-3 lg:grid-cols-6 gap-x-2 gap-y-4 ">
